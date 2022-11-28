7d39d8d661128bd85629cc1b3863ca63471844ac
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-11-28 11:15:58 +0300

    Внесены изменения в решение задачи 2 без факториала

--- sem2.py/task2witnoutf.py
+++ sem2.py/task2witnoutf.py
@@ -3,11 +3,9 @@
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 # Не используйте функцию math.factorial.
 
-number = int(input('Введите число N: '))
 count = 1
-sum = 1
-while count <= number:
-    for i in range(1,number+1):
-        sum = sum*i
-        print(sum, end = ', ')
-        count += 1
+for i in range(int(input('Введите число N: '))):
+    i = i + 1
+    count = i * count
+    
+    print(count, end = ", ")
